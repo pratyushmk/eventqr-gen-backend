38821db590da390595970537404202260908822f
Pratyush Mulpuru <mk.pratyush@gmail.com>
2026-01-29 17:46:20 -0500

    Update Pydantic user models to support password field and create new models for user registration and toekn responses

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 # Pydantic Models(Dataclass)
 class UserCreate(BaseModel):
@@ -7,6 +8,7 @@ class UserCreate(BaseModel):
     location: str
     email: str
     role: str
+    password: str
 
 class UserResponse(BaseModel):
     id: int
@@ -15,6 +17,19 @@ class UserResponse(BaseModel):
     location: str
     email: str
     role: str
+    is_active: bool
 
     class Config:
         from_attributes = True
+
+# Authentication PyDantic Models
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
