728c373452d977482a434a549aa920d8338fad24
Pratyush Mulpuru <mk.pratyush@gmail.com>
2026-01-30 00:57:31 -0500

    Update event field names and change field type to datetime for date field

--- app/models/events.py
+++ app/models/events.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.orm import relationship
+
 from app.db import Base
 
 class Event(Base):
     __tablename__ = "events"
 
     event_id = Column(Integer, primary_key=True, index=True)
-    event_name = Column(String(100), nullable=False)
-    event_date = Column(String, nullable=False)
-    event_location = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    date = Column(DateTime, nullable=False)
+    location = Column(String(100), nullable=False)
 
     tickets = relationship("Ticket", back_populates="events")
 
