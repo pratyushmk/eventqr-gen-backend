2a57e0a63436736fce0fcfaca20ee987b159fa45
Pratyush Mulpuru <mk.pratyush@gmail.com>
2026-01-31 18:16:50 -0500

    Add foreign key enforcement for SQLite

--- app/db.py
+++ app/db.py
@@ -1,7 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Database setup
-engine = create_engine("sqlite:///users.db", connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///event_registrations.db", connect_args={"check_same_thread": False})
+
+# Enable foreign key constraints in SQLite
+@event.listens_for(engine, "connect")
+def enable_sqlite_fk(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
